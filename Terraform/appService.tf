1f093c61b9d3b8f39ae216eff73f0529bc237ac3
Josue Solis <josusols@hotmail.com>
2019-04-27 22:22:31 -0600

    New resources names

--- Terraform/appService.tf
+++ Terraform/appService.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "test" {
-  name     = "AzureBootCampTerraformRg"
+  name     = "FjsolisAzureBootCampTerraformRg"
   location = "eastus2"
 }
 
 resource "azurerm_app_service_plan" "test" {
-  name                = "azbctf-appserviceplan"
+  name                = "Fjsolis-azbctf-appserviceplan"
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
 
@@ -15,7 +15,7 @@ resource "azurerm_app_service_plan" "test" {
 }
 
 resource "azurerm_app_service" "test" {
-  name                = "azbctf-app-service"
+  name                = "Fjsolis-azbctf-app-service"
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
   app_service_plan_id = "${azurerm_app_service_plan.test.id}"
